358440cbdd1e1da364f4d0231d69e9f12cacd878
Admin <globalsmbsolutions@gmail.com>
2022-12-18 10:39:22 -0700

    Added OS Selection

--- NGINX_Installer.py
+++ NGINX_Installer.py
@@ -1,1 +1,29 @@
 # NGINX Install from official Repo
+import subprocess
+
+
+def user_prompt():
+    print('What OS should I install it for (q to quit)?')
+    print('E.X Debian, Windows, Mac')
+    user_input = input('>> ').lower()
+
+    if user_input == 'q':
+        exit()
+
+    print(f"You have selected {user_input}")
+    return user_input
+
+
+def debian_installer():
+    process_output = subprocess.run(['sudo apt install curl gnupg2 ca-certificates lsb-release debian-archive-keyring'])
+    print(process_output)
+
+
+if __name__ == '__main__':
+    while True:
+        output = user_prompt()
+        if output == 'debian':
+            debian_installer()
+        else:
+            print('Sorry that option is not available\n')
+
