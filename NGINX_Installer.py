4bd6bb315d0fe575fa19b0e564b179ac1d33ecb6
Georgio Cha <92047233+SassyBaker@users.noreply.github.com>
2022-12-18 15:38:47 -0700

    Removed Extra Sudo

--- NGINX_Installer.py
+++ NGINX_Installer.py
@@ -28,8 +28,8 @@ def debian_installer():
         'apt install curl gnupg2 ca-certificates lsb-release debian-archive-keyring',
         'curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor | tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null',
         'gpg --dry-run --quiet --no-keyring --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg',
-        'echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] http://nginx.org/packages/debian `lsb_release -cs` nginx" | sudo tee /etc/apt/sources.list.d/nginx.list',
-        'echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" | sudo tee /etc/apt/preferences.d/99nginx',
+        'echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] http://nginx.org/packages/debian `lsb_release -cs` nginx" | tee /etc/apt/sources.list.d/nginx.list',
+        'echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" | tee /etc/apt/preferences.d/99nginx',
         'apt update'
         'apt install nginx'
     ]
